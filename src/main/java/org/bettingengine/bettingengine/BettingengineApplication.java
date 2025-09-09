091b4d266fff864d404551f94048b6dfc87acd4b
Dendeberia Oleksandr <oleksandr.dendeberia@gmail.com>
2025-09-09 09:24:53 +0200

    init

---
+++ src/main/java/org/bettingengine/bettingengine/BettingengineApplication.java
@@ -0,0 +1,13 @@
+package org.bettingengine.bettingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bettingengine/bettingengine/BettingengineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bettingengine.bettingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
